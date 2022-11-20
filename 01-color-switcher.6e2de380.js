9f07355b4d2162ec752ca2a5c07e74ee4b2abe1b
Andriy19990 <106825498+Andriy19990@users.noreply.github.com>
2022-11-20 13:10:43 +0000

    Deploying to gh-pages from @ Andriy19990/goit-js-hw-09@12c595023cd19e951e45f5e1b37e881df8bd1f06 🚀

---
+++ 01-color-switcher.6e2de380.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),n=document.querySelector("body");let a=null;e.addEventListener("click",(function(){e.disabled=!0,a=setInterval((()=>{n.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.addEventListener("click",(function(){e.disabled=!1,clearInterval(a)}));
+//# sourceMappingURL=01-color-switcher.6e2de380.js.map
